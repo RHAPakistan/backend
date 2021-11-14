f49406585543dabc0bf439341d2a321ef2b74d7e
Hassananwar12030 <53085534+Hassananwar12030@users.noreply.github.com>
2021-11-14 03:52:01 -0800

    Update index.js
    added volunteer router

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParsser = require('body-parser');
 const mongoose = require('mongoose');
 const providerRouter = require('./routers/providerRouter');
+const volunteerRouter = require('./routers/volunteerRouter');
 const dotenv = require('dotenv');
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -17,6 +18,7 @@ connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
 })*/
 
+//connecting to mongoDB database
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/rhaDB', {
 
     useNewUrlParser: true,
@@ -27,11 +29,11 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/rhaDB', {
 .then(() => console.log('MongoDB connection established.'))
 .catch((error) => console.error("MongoDB connection failed:", error.message))
 
-
+//routes
 app.use('/api/providers', providerRouter);
-app.get('/', (req, res) => {
-    res.send("Welcome to our ToDo")
-})
+app.use('/api/volunteers', volunteerRouter);
+
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
