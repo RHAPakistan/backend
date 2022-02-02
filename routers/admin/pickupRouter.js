609d21faf768204e0c7a509ca5ff67391f48e0c9
Hassananwar12030 <53085534+Hassananwar12030@users.noreply.github.com>
2022-02-03 03:13:52 +0500

    added get route

--- routers/admin/pickupRouter.js
+++ routers/admin/pickupRouter.js
@@ -11,16 +11,40 @@ const pickupRouter = express.Router();
 
 //get Pickups
 pickupRouter.get(
-    '/',
-    expressAsyncHandler(async (req,res)=>{
-      const pickups = await Pickup.find();
-      if(pickups){
-        res.send({error:0, pickups: pickups});
+  '/',
+  expressAsyncHandler(async (req,res)=>{
+    const pickups = await Pickup.find();
+    if(pickups){
+      res.send({error:0, pickups: pickups});
+    }
+    else{
+      res.status(404).send({error: 1, message: "No pickup found"});
+    }
+  })
+);
+
+//get detials (some missing info)
+pickupRouter.get(
+  '/:id',
+  expressAsyncHandler(async (req,res)=>{
+    const pickup = await Pickup.findById(req.params.id);
+    if (pickup){
+      console.log(pickup.provider.toString());
+      const provider = await Provider.findOne({_id: pickup.provider.toString()});
+      let volunteer = {};
+      let admin = {};
+      if(pickup.volunteer){
+        volunteer = await Volunteer.findOne({_id: pickup.volunteer.toString()});
       }
-      else{
-        res.status(404).send({error: 1, message: "No pickup found"});
+      if(pickup.admin){
+        admin = await Admin.findOne({_id: pickup.admin.toString()});
       }
-    })
-  )
+      res.status(200).send({error: 0, pickupInfo:pickup, provider: provider, volunteer: volunteer, admin: admin});
+    }
+    else{
+      res.status(404).send({error: 1, message: "No pickup found"});
+    }
+  })
+);
 
 module.exports = pickupRouter;
