bc786cc6105a2a1f14500c9854f72e853e7db603
Hassananwar12030 <53085534+Hassananwar12030@users.noreply.github.com>
2022-02-08 03:46:48 +0500

    assign dropoff and volunteer APIs

--- routers/admin/pickupRouter.js
+++ routers/admin/pickupRouter.js
@@ -5,7 +5,7 @@ const Admin = require('../../models/admin');
 const Volunteer = require('../../models/volunteer');
 const Provider = require('../../models/provider');
 const Pickup = require('../../models/pickup');
-const { generateToken, isAuth } = require('../../utils.js');
+const { isAuth } = require('../../utils.js');
 
 const pickupRouter = express.Router();
 
@@ -29,7 +29,7 @@ pickupRouter.get(
   expressAsyncHandler(async (req,res)=>{
     const pickup = await Pickup.findById(req.params.id);
     if (pickup){
-      console.log(pickup.provider.toString());
+      //console.log(pickup.provider.toString());
       const provider = await Provider.findOne({_id: pickup.provider.toString()});
       let volunteer = {};
       let admin = {};
@@ -44,6 +44,41 @@ pickupRouter.get(
     else{
       res.status(404).send({error: 1, message: "No pickup found"});
     }
+  })
+);
+
+//adding a dropoff loc
+pickupRouter.post(
+  '/:id/dropoff/',
+  isAuth,
+  expressAsyncHandler(async (req, res)=>{
+    await Pickup.findByIdAndUpdate(req.params.id, {deliveryAddress: req.body.dropoff});
+    const pickup = await Pickup.findById(req.params.id);
+    if(pickup){
+      res.status(200).send({error: 0, message: "Dropoff assigned sucessfully!", pickup : pickup});
+    }
+    else{
+      res.status(404).send({error: 1, message: "No pickup found"});
+    }
+
+  })
+);
+
+//adding a volunteer
+pickupRouter.post(
+  '/:id/volunteer/',
+  isAuth,
+  expressAsyncHandler(async (req, res)=>{
+    const volunteer = await Volunteer.findById(req.body.volunteer_id);
+    await Pickup.findByIdAndUpdate(req.params.id, {volunteer: volunteer});
+    const pickup = await Pickup.findById(req.params.id);
+    if(pickup){
+      res.status(200).send({error: 0, message: "Volunteer assigned sucessfully!", pickup : pickup});
+    }
+    else{
+      res.status(404).send({error: 1, message: "No pickup found"});
+    }
+
   })
 );
 
