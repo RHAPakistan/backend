cb852393c2cfc0683cea6e62fb599a8ea2e0a620
Hassananwar12030 <53085534+Hassananwar12030@users.noreply.github.com>
2022-02-09 08:23:04 +0500

    add token model

---
+++ models/token.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const tokenSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    otp: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 3600,
+    },
+});
+
+module.exports = mongoose.model("token", tokenSchema);
