af638bb574b502361066a480f1db324ff48a7de3
Hassananwar12030 <53085534+Hassananwar12030@users.noreply.github.com>
2022-04-22 04:46:06 +0500

    making Location of Point schema

--- models/pickup.js
+++ models/pickup.js
@@ -1,5 +1,15 @@
 const  mongoose  = require("mongoose");
-
+const pointSchema = new mongoose.Schema({
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+});
 const pickupSchema = new mongoose.Schema({
     provider: { type: mongoose.Schema.Types.ObjectId, ref: 'provider', required: true },
     admin: { type: mongoose.Schema.Types.ObjectId, ref: 'admin' },
@@ -17,8 +27,11 @@ const pickupSchema = new mongoose.Schema({
     typeOfFood: { type: String },
     broadcast: {type: Boolean},
     status: {type: Number, enum: [0,1,2,3,4,5], required: true,},
-    pickupCoordinate: [{type: Number}]
+    //pickupCoordinate: [{type: Number}]
+    pickupCoordinate: {
+        type: pointSchema,
+    }
 })
-//pickupSchema.index( {  pickupCoordinate : "2dsphere" } )
+pickupSchema.index( {  pickupCoordinate : "2dsphere" } )
 
 module.exports = mongoose.model('Pickup', pickupSchema);
